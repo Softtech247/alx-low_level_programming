8780c3b98cae0258f725ff9e88c4c30c9e15f0d2
Stephen <kalu.stephen.247@gmail.com>
2023-08-11 12:20:42 -0700

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'get the last digit of the number'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 48;
+	int g = 97;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (g <= 102)
+	{
+		putchar(g);
+		g++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'get the last digit of the number'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+	putchar(n);
+	putchar(',');
+	putchar(' ');
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
