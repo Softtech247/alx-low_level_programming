217c08918fe5653795c84ae91b8a06434de51437
Stephen <kalu.stephen.247@gmail.com>
2023-08-11 11:11:33 -0700

    message

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'get the last digit of the number'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+	int g = 65;
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	while (g < 91)
+	{
+		putchar(n);
+		g++;
+	}
+	putchar('\n');
+	return (0);
+}
