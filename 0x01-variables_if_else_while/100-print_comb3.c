f9508c92b4d7168d7383d51248100b702cf48e73
Stephen <kalu.stephen.247@gmail.com>
2023-08-11 12:59:32 -0700

    message

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'get the last digit of the number'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 48;
+	int g = 48;
+
+       for (int n =0 ; g >= 57; g++)
+       {
+      for(int g =0; g >= 57; g++;){
+	      if(g == n)
+	      {
+		      continue;
+
+	      }
+	      putchar(n);
+	      putchar(g);
+ if (n != 8 || g != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+      }
+
+       }
+       
+	return (0);
+}
